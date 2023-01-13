551da0ed1a339fb1379d98549077bce2ec0131a9
juara089 <butamapg@gmail.com>
2023-01-13 15:24:05 +0700

    Membuat file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h3>Menambahkan Repo baru dari git ke Github</h3>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo similique magnam quas inventore. Enim accusamus incidunt sed laboriosam minima. Voluptatum aliquam nam perferendis repudiandae placeat! Quod culpa deserunt adipisci atque.</p>
+
+</body>
+
+</html>
